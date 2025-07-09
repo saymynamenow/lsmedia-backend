e6649f4c84e30d25f983c6f2787b14f8ea904cda
saymynamenow <muhammadfiqriananda@student.telkomuniversity.ac.id>
2025-07-09 23:01:34 +0800

    [U]

--- index.js
+++ index.js
@@ -63,8 +63,8 @@ app.use(
   })
 );
 app.use(compression());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 
 // Serve static files from uploads directory
